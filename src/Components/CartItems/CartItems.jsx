6af85ec75f71e6e07fe25eba9a11f4f62e18ccd0
Hawa Jeme <kuuzey46@gmail.com>
2024-03-16 21:32:11 +0300

    cart page and cart items

---
+++ src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react'
+import './CartItems.css'
+import deleteIcon from '../Assets/delete-icon.png'
+import { DataContext } from '../../Context/DataContext'
+
+export const CartItems = () => {
+    const {allProducts, cartItems, removeFromCart} = useContext(DataContext)
+  return (
+    <div className='cartItems'>
+        <div className='cartItems-options'>
+            <p>Products</p>
+            <p>Title</p>
+            <p>Price</p>
+            <p>Quantity</p>
+            <p>Total</p>
+            <p>Remove</p>
+        </div>
+        <hr/>
+        {allProducts.map(e => {
+            if(cartItems[e.id] > 0){
+            return (
+                <div>
+                    <div className='cartItem-format'>
+                        <img src={e.image} alt='' className=''/>
+                        <p>{e.title}</p>
+                        <p>SR{e.price}</p>
+                        <button className='quantity'>{cartItems[e.id]}</button>
+                        <p>{e.price*cartItems[e.id]}</p>
+                        <img src={deleteIcon} onClick={()=>{removeFromCart(e.id)}} className=''></img>
+                    </div>
+                    <hr/>
+                </div>)
+            }
+        })}
+    </div>
+  )
+}
